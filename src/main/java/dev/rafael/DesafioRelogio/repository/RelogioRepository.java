7ef397e323025b391a3de66d99f3d45d825c7f69
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-05-26 14:38:16 -0300

    feature: Criando o Repository

---
+++ src/main/java/dev/rafael/DesafioRelogio/repository/RelogioRepository.java
@@ -0,0 +1,12 @@
+package dev.rafael.DesafioRelogio.repository;
+
+import dev.rafael.DesafioRelogio.entity.Relogio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RelogioRepository extends JpaRepository<Relogio, UUID>, JpaSpecificationExecutor<Relogio> {
+}
