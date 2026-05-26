08652c86f929557eeb20151fd7f61863f19152ba
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-05-26 13:38:02 -0300

    first commit

---
+++ src/test/java/dev/rafael/DesafioRelogio/DesafioRelogioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafael.DesafioRelogio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioRelogioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
